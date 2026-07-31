068712ae5139089c109a39381c741d91e9d8c7b1
Siddhi1135 <siddhimane6320@gmail.com>
2026-07-31 18:15:56 +0530

    Day 1: Python variables,lists and loops

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+name="Siddhi"
+age=21
+print(name)
+print(age)
+destinations=["Bali","Paris", "Tokyo", "Dubai"]
+print(destinations)
+print(destinations[0])
+print(destinations[1])
+print(len(destinations))
+for place in destinations:
+    print(place)
